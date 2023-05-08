804243f68201c7d8bf3054493aa5d6b6706ccc88
Florian Simon <florian.simon@protonmail.com>
2023-05-08 02:33:37 -0400

    I'm not intersted in building libutils tests or benchmarks

--- scripts/copy-sources.sh
+++ scripts/copy-sources.sh
@@ -16,7 +16,8 @@ cp -R ${BASEDIR}/aosp-source/build/tools/zipalign/*.h ${BASEDIR}/src/zipalign
 
 cp -R ${BASEDIR}/aosp-source/core/libutils/*.cpp ${BASEDIR}/src/libutils/
 cp -R ${BASEDIR}/aosp-source/core/libutils/include/ ${BASEDIR}/include/libutils
-rm -rf ${BASEDIR}/src/libutils/*_test.cpp
+rm -rf ${BASEDIR}/src/libutils/*_benchmark.cpp
+rm -rf ${BASEDIR}/src/libutils/*_test*.cpp
 rm -rf ${BASEDIR}/src/libutils/*_fuzz.cpp
 
 cp -R ${BASEDIR}/aosp-source/logging/liblog/include/ ${BASEDIR}/include/liblog/
